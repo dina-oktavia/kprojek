7d6ef66e4b3d903c3ddcf867d13be774f3501db8
Quenzell <61855248+Quenzell@users.noreply.github.com>
2020-09-11 14:45:45 +0700

    update percentage

--- resources/views/percentage.blade.php
+++ resources/views/percentage.blade.php
@@ -15,6 +15,23 @@
   .card{
       background-color: #F5F5F5;
   }
+  .kata p{
+    font-weight: bold;
+    font-size: 15px;
+    margin-left: 30px;
+    margin-top: 20px;
+  }
+  .dropdown{
+    margin-left: 80px;
+    margin-top: -25px;
+  }
+  .btn{
+    background-color: #63D0FF;
+    height: 30px;
+    font-size: 13px;
+    color: white;
+    font-weight: bold;
+  }
 </style>
 @endsection
 
@@ -35,4 +52,41 @@
             </a>
             </nav>
 
+<!-- isinya -->
+      <div class="kata">
+          <p style=" margin-bottom: -15px;">Years
+          <div class="dropdown">
+                <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                2019-2020
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </div>          
+          </p>
+          <p>Grade
+          <div class="dropdown" style=" margin-top: -40px;">
+                <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Odd
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </div>   
+          </p>
+      </div>
+
+<!-- table -->
+
+<!-- btn -->
+            <div class="save">
+            <button type="button" class="btn btn-sm" style="background-color: #63D0FF">Download
+              <img src="{{ url('/dashboard/dist/img/direct.png') }}" style="width: 15px; margin-left:7px; margin-top: -5px;">
+            </button>
+            </div>
+                    
 @endsection
